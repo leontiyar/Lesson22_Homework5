dc3206c76c64671e6c86991982a38764a48dccac
Leontiyar <vthehang@gmail.com>
2021-07-26 13:59:37 +0300

    Добавил коментарии для более понятной читки кода

--- Homework5/Program.cs
+++ Homework5/Program.cs
@@ -1,14 +1,25 @@
 ﻿using System;
-//Вывод 4 треушольников
+//Вывод 4 треугольников
 namespace Homework5
 {
     class Program
     {
         static void Main(string[] args)
         {
-            for (int i = 1; i <= 10; i++)
+            for (int i = 1; i <= 10; i++)//Проходим по строкам от 1 до 10
             {
-                for (int j = 1; j <= i; j++)
+                for (int j = 1; j <= i; j++)//Проходим по символам строки от 1 до і
+                {
+                    Console.Write("#");//Отрисовываем #
+                }
+                Console.WriteLine();//В конце переходим на новую строку
+            }
+            Console.WriteLine();//Делаем отступ
+            Console.WriteLine();
+            for (int i = 1; i <= 10; i++)//Снова проходимся по строкам
+            {
+                for (int j = 10; j >= i; j--)//Сейчас же запускаем обратный цикл,
+                //т.к. количество # убывает от 10 до 1
                 {
                     Console.Write("#");
                 }
@@ -18,21 +29,13 @@ namespace Homework5
             Console.WriteLine();
             for (int i = 1; i <= 10; i++)
             {
-                for (int j = 10; j >= i; j--)
-                {
-                    Console.Write("#");
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine();
-            Console.WriteLine();
-            for (int i = 1; i <= 10; i++)
-            {
-                for (int j = 1; j <= 10-i; j++)
+                //В этот раз запускаем 2 последовательных цикла
+                for (int j = 1; j <= 10-i; j++)//1 отрисовывает пробелы (количество пробелов на 1 строке - 9)
+                //,на 2 строке - 8 и на каждой 10-i
                 {
                     Console.Write(" ");
                 }
-                for (int j = 1; j <= i; j++)
+                for (int j = 1; j <= i; j++)//а 2 - #(а количество решеток = 10 - (10 - i) = 10 - 10 + i = i)
                 {
                     Console.Write("#");
                 }
@@ -42,16 +45,17 @@ namespace Homework5
             Console.WriteLine();
             for (int i = 1; i <= 10; i++)
             {
-                for (int j = 9; j > 10-i; j--)
+                for (int j = 9; j > 10-i; j--)//здесь к-ство пробелов увеличивается
                 {
                     Console.Write(" ");
                 }
                 for (int j = 10; j >= i; j--)
                 {
-                    Console.Write("#");
+                    Console.Write("#");//а к-ство # уменьшается
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine();
         }
     }
 }
