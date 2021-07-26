66d9447709288ea9d63448c55371979d403cff25
Leontiyar <vthehang@gmail.com>
2021-07-26 21:39:36 +0300

    Добавил выбор размера треугольников и защиту от дурака

--- Homework5/Program.cs
+++ Homework5/Program.cs
@@ -6,56 +6,67 @@ namespace Homework5
     {
         static void Main(string[] args)
         {
-            for (int i = 1; i <= 10; i++)//Проходим по строкам от 1 до 10
+            Console.WriteLine("Введите размеры треугольников :");
+            bool smart = int.TryParse(Console.ReadLine(), out int size);
+            if (smart && size > 1)
             {
-                for (int j = 1; j <= i; j++)//Проходим по символам строки от 1 до і
+                Console.WriteLine();
+                Console.WriteLine();
+                for (int i = 1; i <= size; i++)//Проходим по строкам от 1 до 10
                 {
-                    Console.Write("#");//Отрисовываем #
+                    for (int j = 1; j <= i; j++)//Проходим по символам строки от 1 до і
+                    {
+                        Console.Write("#");//Отрисовываем #
+                    }
+                    Console.WriteLine();//В конце переходим на новую строку
                 }
-                Console.WriteLine();//В конце переходим на новую строку
-            }
-            Console.WriteLine();//Делаем отступ
-            Console.WriteLine();
-            for (int i = 1; i <= 10; i++)//Снова проходимся по строкам
-            {
-                for (int j = 10; j >= i; j--)//Сейчас же запускаем обратный цикл,
-                //т.к. количество # убывает от 10 до 1
+                Console.WriteLine();//Делаем отступ
+                Console.WriteLine();
+                for (int i = 1; i <= size; i++)//Снова проходимся по строкам
                 {
-                    Console.Write("#");
+                    for (int j = size; j >= i; j--)//Сейчас же запускаем обратный цикл,
+                    //т.к. количество # убывает от 10 до 1
+                    {
+                        Console.Write("#");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+                for (int i = 1; i <= size; i++)
+                {
+                    //В этот раз запускаем 2 последовательных цикла
+                    for (int j = 1; j <= size - i; j++)//1 отрисовывает пробелы (количество пробелов на 1 строке - 9)
+                    //,на 2 строке - 8 и на каждой 10-i
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int j = 1; j <= i; j++)//а 2 - #(а количество решеток = 10 - (10 - i) = 10 - 10 + i = i)
+                    {
+                        Console.Write("#");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+                for (int i = 1; i <= size; i++)
+                {
+                    for (int j = size - 1; j > size - i; j--)//здесь к-ство пробелов увеличивается
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int j = size; j >= i; j--)
+                    {
+                        Console.Write("#");//а к-ство # уменьшается
+                    }
+                    Console.WriteLine();
                 }
                 Console.WriteLine();
             }
-            Console.WriteLine();
-            Console.WriteLine();
-            for (int i = 1; i <= 10; i++)
+            else
             {
-                //В этот раз запускаем 2 последовательных цикла
-                for (int j = 1; j <= 10-i; j++)//1 отрисовывает пробелы (количество пробелов на 1 строке - 9)
-                //,на 2 строке - 8 и на каждой 10-i
-                {
-                    Console.Write(" ");
-                }
-                for (int j = 1; j <= i; j++)//а 2 - #(а количество решеток = 10 - (10 - i) = 10 - 10 + i = i)
-                {
-                    Console.Write("#");
-                }
-                Console.WriteLine();
+                Console.WriteLine("Нужно вводить числа, которые больше 1!");
             }
-            Console.WriteLine();
-            Console.WriteLine();
-            for (int i = 1; i <= 10; i++)
-            {
-                for (int j = 9; j > 10-i; j--)//здесь к-ство пробелов увеличивается
-                {
-                    Console.Write(" ");
-                }
-                for (int j = 10; j >= i; j--)
-                {
-                    Console.Write("#");//а к-ство # уменьшается
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine();
         }
     }
 }
