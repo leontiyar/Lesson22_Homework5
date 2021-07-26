2788eeccfa18cf0ae50763aaecf61d91340a0f22
Leontiyar <vthehang@gmail.com>
2021-07-26 13:44:27 +0300

    Добавьте файлы проекта.

---
+++ Homework5/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+//Вывод 4 треушольников
+namespace Homework5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 10; j >= i; j--)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10-i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 9; j > 10-i; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 10; j >= i; j--)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Lesson22/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+//вложенные циклы
+namespace Lesson22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите высоту прямоугольника: ");
+            int height = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите ширину прямоугольника: ");
+            int width = int.Parse(Console.ReadLine());
+
+            for (int j = 0; j < height; j++)
+            {
+                for (int i = 0; i < width; i++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+            
+        }
+    }
+}
